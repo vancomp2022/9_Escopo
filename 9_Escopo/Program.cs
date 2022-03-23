2354c87758cbe258d49fece6d9cfdcf7ee1e9c05
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-23 19:34:15 -0300

    Adicionar arquivos de projeto.

---
+++ 9_Escopo/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 8 - Condicionais2");
+
+            int idadeJoao = 18;
+            int quantidadePessoas = 2;
+
+            //bool acompanhado = quantidadePessoas >= 2;
+
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Joao esta acompanhado.";
+            }
+            else
+            {
+                mensagemAdicional = "Joao nao esta acompanhado.";
+            }
+
+            int idade = 68;
+            bool ehIdoso = idade >= 65;
+
+            Console.WriteLine(ehIdoso);
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Nao pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
